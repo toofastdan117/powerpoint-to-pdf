5e858f8b6aad85c86e2d97c4728202d149de0bb1
toofastdan117 <81544118+toofastdan117@users.noreply.github.com>
2023-12-15 18:55:37 -0600

    add documentation

---
+++ README.md
@@ -0,0 +1,7 @@
+# Powerpoint to PDF Converter
+
+Converts a directory that contains powerpoint files (.pptx & .ppt) and converts them to PDFs.
+
+Afterwards, it deletes all powerpoint files to save space (make sure to back up before running).
+
+Relies on the ppt2pdf python command line tool: https://pypi.org/project/ppt2pdf/
--- powerpoint_to_pdf.sh
+++ powerpoint_to_pdf.sh
@@ -1,9 +1,12 @@
 # File for converting all powerpoints (.pptx & .ppt) to PDFs.
 
+# pasting a user input file directory
 echo "Paste a filepath to a directory where you want all the powerpoints (.pptx, .ppt) to be converted to PDFs:"
 read -r filepath
 echo Your file path is: "$filepath"
 cd "$filepath"
+
+# converting all powerpoints to pdfs using the ppt2pdf python package and removing all old powerpoint files
 ppt2pdf dir .
 rm *.pptx
 rm *.ppt
